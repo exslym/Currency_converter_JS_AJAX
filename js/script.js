8b12594eccab565ab1b44175590108848fac25a3
exslym <76844562+exslym@users.noreply.github.com>
2021-08-05 21:29:14 +0300

    add promise function

--- js/script.js
+++ js/script.js
@@ -3,6 +3,39 @@ let inputRub = document.getElementById('rub'),
 	inputUsd = document.getElementById('usd');
 
 inputRub.addEventListener('input', () => {
+	function convert() {
+		return new Promise(function (resolve, reject) {
+			let request = new XMLHttpRequest();
+			request.open('GET', 'js/current.json');
+			request.setRequestHeader(
+				'Content-type',
+				'application/json; charset=utf-8'
+			);
+			request.send();
+
+			request.onload = function () {
+				if (request.readyState === 4) {
+					if (request.status == 200) {
+						resolve(this.response);
+					} else {
+						reject();
+					}
+				}
+			};
+		});
+	}
+	convert()
+		.then(response => {
+			let data = JSON.parse(response);
+			inputUsd.value = inputRub.value / data.usd;
+		})
+		.catch(() => (inputUsd.value = 'Something go wrong!'));
+});
+
+/* 	let inputRub = document.getElementById('rub'),
+	inputUsd = document.getElementById('usd');
+
+	inputRub.addEventListener('input', () => {
 	let request = new XMLHttpRequest();
 
 	request.open('GET', 'js/current.json');
@@ -28,4 +61,4 @@ inputRub.addEventListener('input', () => {
 			inputUsd.value = 'Something go wrong!';
 		}
 	});
-});
+}); */
